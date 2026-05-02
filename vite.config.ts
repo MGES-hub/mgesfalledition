c60fb5f418cec40223bdb65ff84293de35e9bfa4
MGES-hub <mges.organizers@gmail.com>
2026-05-02 17:35:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import { reactStart } from "@tanstack/react-start/vite";
+import { tanstackStartVite } from "@tanstack/start/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    cloudflare(),
     tanstackRouter(),
-    reactStart(),
+    tanstackStartVite(),
+    cloudflare(),
     tailwindcss(),
     tsconfigPaths(),
   ],
