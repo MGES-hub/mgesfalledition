58716c6767239e66965a9f35fd310370a56b5789
MGES-hub <mges.organizers@gmail.com>
2026-05-02 18:35:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  build: {
+    outDir: "dist/client",
+  },
   plugins: [
     TanStackRouterVite(),
     react(),
